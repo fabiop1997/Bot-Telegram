edd6e5f97420ffff5c105a5b9e736c8a558f53f2
Fábio Paulo <fabiopauloect@gmail.com>
2025-08-24 23:07:55 -0300

    Implementação de Funções para Identifcar Audio ou Texto

--- telegram-bot.py
+++ telegram-bot.py
@@ -13,15 +13,26 @@ load_dotenv()
 
 bot = telebot.TeleBot(os.getenv("API_KEY_TELEGRAM"))
 
-@bot.message_handler(func=lambda msg:True)
+@bot.message_handler(content_types= ['audio','voice'])
+def transcreve_audio(msg: telebot.types.Message):
+
+    texto_respota = "isso é uma transcrição"
+   
+    bot.reply_to(msg,texto_respota)
 
 
+
+
+@bot.message_handler(content_types=['text'])
 def captura_mensagem(msg: telebot.types.Message):
+
+    
+
     texto = msg.text
 
     texto_respota = modelo_linguagem(texto)
 
-    bot.reply_to(None,texto_respota)
+    bot.reply_to(msg,texto_respota)
 
 
     
